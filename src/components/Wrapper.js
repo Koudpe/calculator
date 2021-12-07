e304597ae6b5db8d16f82dc634d001b5282da8b8
Petr Koudelka <koudy@Petr-MacBook-Pro.local>
2021-12-07 11:32:33 +0100

    init

---
+++ src/components/Wrapper.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./css/Wrapper.css"
+
+const Wrapper = ({ children}) => {
+    return(
+        <div className="wrapper">
+            {children}
+        </div>
+    );
+}
+
+export default Wrapper
